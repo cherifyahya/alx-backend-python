b5b7989c0e28137abcedaf90d7de44af7dc25d2a
yahya <cherifyahya069@gmail.com>
2024-08-19 23:05:45 +0000

    async func

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Write an asynchronous coroutine"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """return a random value after wait random seconds
+      between 0 and max_delay"""
+    if max_delay < 0:
+        raise ValueError("max_delay must be a positive number")
+
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+
+    return delay
